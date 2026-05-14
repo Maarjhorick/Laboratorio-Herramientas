09a5399f2278384b9a17131fdae88f50768350cd
StefanoBravo <stefano.teofanos.bravo.rosales@gmail.com>
2026-05-13 19:48:55 -0500

    Ingreso de Notas

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,7 +2,7 @@
 const verifyDocente = (req, res, next) => {
     const token = req.headers['authorization'];
     if (!token) return res.status(403).send("Token requerido");
-    // Lógica de verificación...
+
     next();
 };
 
